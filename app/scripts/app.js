0676dd9fcd6cde23044c43fc0139e992de3b873b
Captain Cake <captaincake8@gmail.com>
2014-02-28 15:03:23 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('frontendTutorialApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
